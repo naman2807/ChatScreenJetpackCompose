8ac6e6e0f068fe0acd6a5be81d047ccfdaff2994
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-26 22:24:12 +0530

    Added Image in row to MessageCard

--- app/src/main/java/com/example/chatscreenjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/chatscreenjetpackcompose/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.chatscreenjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.chatscreenjetpackcompose.ui.theme.ChatScreenJetpackComposeTheme
 
@@ -21,6 +24,9 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun MessageCard(message: Message) {
+        Row() {
+           Image(painter = painterResource(id = R.drawable.profile), contentDescription ="profile" )
+        }
         Column() {
             Text(text = "${message.author}!")
             Text(text = "${message.content}!")
