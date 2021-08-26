a637e28a741ad56bcdf7ab9e860ee561c583d722
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-26 22:34:44 +0530

    Added color to image border and text

--- app/src/main/java/com/example/chatscreenjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/chatscreenjetpackcompose/MainActivity.kt
@@ -4,35 +4,53 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.chatscreenjetpackcompose.ui.theme.ChatScreenJetpackComposeTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           MessageCard(message = Message("Colleague", "First Chat"))
+            MessageCard(message = Message("Colleague", "First Chat"))
         }
     }
 
     @Composable
     fun MessageCard(message: Message) {
-        Row() {
-           Image(painter = painterResource(id = R.drawable.profile), contentDescription ="profile" )
-            Column() {
-                Text(text = "${message.author}!")
-                Text(text = "${message.content}!")
+        // Add padding around our message
+        Row(modifier = Modifier.padding(all = 8.dp)) {
+            Image(
+                painter = painterResource(R.drawable.profile),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    // Set image size to 40 dp
+                    .size(40.dp)
+                    // Clip image to be shaped as a circle
+                    .clip(CircleShape)
+                    .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+            )
+
+            // Add a horizontal space between the image and the column
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Column {
+                Text(text = message.author,
+                color = MaterialTheme.colors.primary
+                )
+                // Add a vertical space between the author and message texts
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = message.content)
             }
         }
-
-
     }
 
 
@@ -43,5 +61,5 @@ class MainActivity : ComponentActivity() {
 
     }
 
-    data class Message(val author: String, val content:String)
+    data class Message(val author: String, val content: String)
 }
