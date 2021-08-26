673a343b1d7bdc47f7e79a4ef6860beec6aa8b95
naman2807 <namanagarwaldbs438@gmail.com>
2021-08-26 21:47:33 +0530

    Added Text composable

--- app/src/main/java/com/example/chatscreenjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/chatscreenjetpackcompose/MainActivity.kt
@@ -14,25 +14,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ChatScreenJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+           Text(text ="Hello World")
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ChatScreenJetpackComposeTheme {
-        Greeting("Android")
     }
 }
